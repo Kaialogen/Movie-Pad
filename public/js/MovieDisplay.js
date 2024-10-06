7da71d315346784fe011aa97e2292db850e19de8
Kaialogen <kaialogen@protonmail.com>
2024-10-06 18:33:32 +0100

    Massive Cleanup

---
+++ public/js/MovieDisplay.js
@@ -0,0 +1,16 @@
+    // Takes the ID from the URL and loads all the data into the DOM
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const movieId = parseInt(urlParams.get("id"));
+    const movie = movies.find((movie) => movie.id === movieId);
+
+    document.getElementById("days-to-rent-x").id = "days-to-rent-" + movieId.toString(); 
+    document.getElementById("movie-title").textContent = movie.name;
+    document.getElementById("movie-image").src = movie.image;
+    document.getElementById("movie-genre").textContent = movie.genre;
+    document.getElementById("movie-director").textContent = movie.director;
+    document.getElementById("movie-actors").textContent = movie.actors;
+    document.getElementById("movie-description").textContent = movie.description;
+    document.getElementById("movie-release-date").textContent = movie.releaseDate;
+    document.getElementById("movie-price").textContent = "£" + (movie.price).toFixed(2);
+    document.getElementById("movie-video").src = movie.video;
--- public/js/help.js
+++ public/js/help.js
@@ -1,12 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i = 0;
+let acc = document.getElementsByClassName("accordion");
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-
     this.classList.toggle("active");
-
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
