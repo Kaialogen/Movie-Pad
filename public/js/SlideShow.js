745c84c1e7f46ed927f8693224617c213d4db738
Kaialogen <kaialogen@protonmail.com>
2025-05-20 19:19:56 +0100

    feat: Fixed automatic slideshow and removed manual slideshow control

--- public/js/SlideShow.js
+++ public/js/SlideShow.js
@@ -1,42 +1,20 @@
 let slideIndex = 0;
 
-//Function calls for automatic and manual slideshows;
-showSlidesManual(slideIndex);
-showSlidesAutomatic(slideIndex);
-
-
-//Function for manual slideshow index change
-function plusSlides(value) {
-    slideIndex += value;
-    showSlidesManual(slideIndex);
-}
-
-//Function for automatic slideshow index change
-function showSlidesManual(n) {
+// Function for automatic slideshow index change
+function showSlidesAutomatic() {
     let slides = document.getElementsByClassName("mySlides");
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
 
-    for (const element of slides) {
-        element.style.display = "none";
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-function showSlidesAutomatic(slideIndex) {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let j = 0; j < slides.length; j++) {
-        slides[j].style.display = "none";
-    }
+    
     slideIndex++;
+
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlidesAutomatic, 10000); // Change image every 10 seconds
 }
+
+showSlidesAutomatic();
