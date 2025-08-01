2bb347a68bf8c3c2d2e293b6c42e3346b2114252
Kaialogen <kaialogen@protonmail.com>
2025-08-01 14:29:45 +0100

    feat: Made the searchbar more React like and migrated to Tailwind

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,68 +1,92 @@
 import { Link } from 'react-router';
-
-import './NavBar.css';
-import submitted from './search';
+import { useState } from 'react';
+import Searchbar from '../Searchbar/Searchbar';
 
 export default function NavBar() {
-  // Function to handle mouse enter event for the dropdown
-  const handleMouseEnter = () => {
-    const dropdownContent = document.getElementById('dropdown-content');
-    if (dropdownContent) {
-      dropdownContent.style.display = 'block';
-    }
-  };
-  // Function to handle mouse leave event for the dropdown
-  const handleMouseLeave = () => {
-    const dropdownContent = document.getElementById('dropdown-content');
-    if (dropdownContent) {
-      dropdownContent.style.display = 'none';
-    }
-  };
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <>
-      <div className='topnav'>
-        <nav>
-          <Link to='/basket' className='basket'>
-            <i className='fa-solid fa-shopping-basket' />
-          </Link>
-          <Link to='/login' className='user'>
-            <i className='fa-solid fa-user' />
-          </Link>
-          <Link to='/help' className='help'>
-            <i className='fa-solid fa-question' />
-          </Link>
-          <Link to='/categories' className='Genre' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <i className='fa-solid fa-bars' />
-          </Link>
-          <div className='homebtn'>
-            <Link to='/' className='homebtn-btn'>
-              <img src='images/FullName.png' className='logo' width={180} height={44} alt='Home Button' />
+    <div className='fixed top-0 w-full z-10 bg-[#1b2530] shadow-md'>
+      <nav className='flex items-center justify-between px-4'>
+        {/* Left: Home Button */}
+        <div className='flex items-center'>
+          <div className='relative left-2 top-3'>
+            <Link to='/' className='homebtn-btn text-white bg-transparent font-sans cursor-pointer'>
+              <img
+                src='images/FullName.png'
+                className='inline-block relative left-1/2 -translate-x-1/2'
+                width={180}
+                height={44}
+                alt='Home Button'
+              />
             </Link>
           </div>
-          <form className='search-bar' method='post' onSubmit={submitted} role='search'>
-            <i className='fa fa-fw fa-search search-icon' />
-            <input className='search-text text-white' type='text' id='query' placeholder='Search' />
-          </form>
-        </nav>
+        </div>
 
-        <div className='dropdown'>
-          <div className='dropdown-content' id='dropdown-content'>
-            <button>Action</button>
-            <button>Adventure</button>
-            <button>Animation</button>
-            <button>Comedy</button>
-            <button>Crime</button>
-            <button>Drama</button>
-            <button>Fantasy</button>
-            <button>Horror</button>
-            <button>Romance</button>
-            <button>Science Fiction</button>
-            <button>Thriller</button>
-            <button>Western</button>
+        <Searchbar />
+
+        {/* Right: Nav Links */}
+        <div className='flex items-center space-x-2'>
+          <Link
+            to='/basket'
+            className='basket bg-purple-700 text-white text-3xl px-4 py-3 rounded-none outline-none border-none hover:bg-purple-500 transition'
+          >
+            <i className='fa-solid fa-shopping-basket' />
+          </Link>
+          <Link
+            to='/login'
+            className='user text-white text-3xl px-4 py-3 rounded-none outline-none border-none hover:bg-[#a9a9a941] transition'
+          >
+            <i className='fa-solid fa-user' />
+          </Link>
+          <Link
+            to='/help'
+            className='help text-white text-3xl px-4 py-3 rounded-none outline-none border-none relative hover:bg-[#a9a9a941] transition'
+          >
+            <i className='fa-solid fa-question' />
+          </Link>
+          {/* Genre Dropdown */}
+          <div
+            className='relative'
+            onMouseEnter={() => setDropdownOpen(true)}
+            onMouseLeave={() => setDropdownOpen(false)}
+          >
+            <Link
+              to='/categories'
+              className='Genre text-white text-3xl px-4 py-3 rounded-none outline-none border-none hover:bg-[#a9a9a941] transition'
+            >
+              <i className='fa-solid fa-bars' />
+            </Link>
+            <div
+              className={`dropdown-content absolute right-0 mt-2 bg-[#1b2530] shadow-lg rounded z-50 min-w-[180px] ${
+                dropdownOpen ? 'block' : 'hidden'
+              }`}
+            >
+              {[
+                'Action',
+                'Adventure',
+                'Animation',
+                'Comedy',
+                'Crime',
+                'Drama',
+                'Fantasy',
+                'Horror',
+                'Romance',
+                'Science Fiction',
+                'Thriller',
+                'Western',
+              ].map((genre) => (
+                <button
+                  key={genre}
+                  className='w-full text-left px-4 py-3 text-white bg-[#1b2530] border-none hover:bg-[#a9a9a941] transition'
+                >
+                  {genre}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </nav>
+    </div>
   );
 }
