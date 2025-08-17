57eaed34c828f9236ba4702a38bf972559498bf8
Kaialogen <kaialogen@protonmail.com>
2025-08-17 16:55:22 +0100

    feat: Enhance login functionality with error handling and API integration

--- Users Backend/server.js
+++ Users Backend/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
 
 // Secret key for JWT
 const SECRET_KEY = process.env.JWT_SECRET || "your-secret-key";
