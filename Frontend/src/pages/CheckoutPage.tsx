a6f9b7a69f5b4d25e6fdb0cccdbee0a693b214dd
Kaialogen <kaialogen@protonmail.com>
2025-09-08 11:50:49 +0100

    feat: Initial implementation of checkout authorisation

--- Frontend/src/pages/CheckoutPage.tsx
+++ Frontend/src/pages/CheckoutPage.tsx
@@ -1,8 +1,26 @@
 import BasketWidget from '../components/BasketWidget/BasketWidget.tsx';
 import CheckoutForm from '../components/CheckoutForm/CheckoutForm.tsx';
+import { useState, useEffect } from 'react';
 
 export default function CheckoutPage() {
-  return (
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const response = await fetch('http://localhost:3000/api/profile', {
+        method: 'GET',
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    };
+    checkLoginStatus();
+  }, []);
+  return loggedIn ? (
     <main className='flex-grow pt-20'>
       <h1 className='text-center text-4xl font-bold mb-12 text-slate-50'>Checkout</h1>
 
@@ -14,6 +32,10 @@ export default function CheckoutPage() {
           <BasketWidget />
         </div>
       </div>
+    </main>
+  ) : (
+    <main className='flex-grow pt-20'>
+      <h1 className='text-center text-4xl font-bold mb-12 text-slate-50'>Please log in to proceed to checkout.</h1>
     </main>
   );
 }
